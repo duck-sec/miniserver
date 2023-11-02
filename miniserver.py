61adbcdd4b65b3a0d6f82e4db7e328b52e58757e
Ducksec <duck@ducksec.io>
2023-11-02 14:56:36 +0000

    Fixed path error when traversing directories

--- miniserver.py
+++ miniserver.py
@@ -1,6 +1,12 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import os
+import argparse
+
+# Accept some arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-sf', '--servefiles', type=bool, required=False, help="Serve files in the directory miniserver runs in - False by default")
+args = parser.parse_args()
 
 # Define ANSI escape codes for text colors
 RED = "\033[91m"
@@ -10,24 +16,28 @@ RESET = "\033[0m"
 # Set a flag to determine the behavior of the root path
 serve_files = False
 
+if args.servefiles:
+    serve_files = args.servefiles
+    print("Serving files from this directory")
+
 def log_and_print(message, color=GREEN):
     tag = GREEN + "[LOG] " + RESET
     # Log the message to a file
-    with open('server.log', 'a') as log_file:
+    with open('log/server.log', 'a') as log_file:
         log_file.write(tag + message + '\n')
-    
     # Print the message to the console with the specified color
     print(tag + message)
 
 class CustomRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global serve_files  # Access the global flag
+        requested_page = self.path.lstrip('/')
 
         if self.path == '/':
             if serve_files:
                 # List files in the directory
                 file_list = os.listdir('.')
-                html_response = '<html><body><h2>Files in the directory:</h2><ul>'
+                html_response = '<html><body><h2>Miniserver:</h2><ul>'
                 for file in file_list:
                     html_response += f'<li><a href="{file}">{file}</a></li>'
                 html_response += '</ul></body></html>'
@@ -36,7 +46,7 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(html_response.encode('utf-8'))
-                log_and_print(f"GET request received for {self.path} - File list served.")
+                log_and_print(f"GET request received for {self.path} - Directory listing served.")
                 return  # Place the return statement inside the if block
 
             self.send_response(200)
@@ -46,15 +56,29 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
             log_and_print(f"GET request received from {self.client_address[0]} for {self.path}")
         else:
             # Check if the requested page exists
-            requested_page = self.path.lstrip('/')
             if os.path.exists(requested_page):
-                # Page exists, send the content
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                with open(requested_page, 'rb') as file:
-                    self.wfile.write(file.read())
-                log_and_print(f"GET request received for {self.path} - Page served.")
+                if os.path.isdir(requested_page):
+                    # Handle directory listing
+                    file_list = os.listdir(requested_page)
+                    directory = self.path  # Use self.path as the directory
+                    html_response = (f"<html><body><h2>Miniserver - Directory Listing For {directory}:</h2><ul>")
+                    for file in file_list:
+                        html_response += f'<li><a href="{os.path.join(directory, file)}">{file}</a></li>'
+                    html_response += '</ul></body></html>'
+                    
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(html_response.encode('utf-8'))
+                    log_and_print(f"GET request received for {self.path} - Directory listing served.")
+                else:
+                    # Page exists, send the content
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    with open(requested_page, 'rb') as file:
+                        self.wfile.write(file.read())
+                    log_and_print(f"GET request received for {self.path} - Page served.")
             else:
                 # Page does not exist, return a 404 Not Found response
                 self.send_response(404)
@@ -71,7 +95,7 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
         
         # Create a filename based on the client's IP address and timestamp
         timestamp = time.strftime("%d-%m-%Y@%H:%M:%S")
-        filename = f"{client_ip}_{timestamp}.txt"
+        filename = f"data/{client_ip}_{timestamp}.txt"
 
         title = "POST request from:" + client_ip + "@" + timestamp
         with open(filename, 'w') as file:
@@ -89,7 +113,7 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(f'[LOG] POST data saved to file: {filename}'.encode("utf-8"))
+        self.wfile.write(f"200-OK".encode("utf-8"))
         log_and_print(f"POST request received from {client_ip} and saved to file: {filename}")
 
 def run(server_class=HTTPServer, handler_class=CustomRequestHandler, port=8000):
